78ce72905013bdf26ba369dc4f374cd931503561
Javier Ocampo <ferroxido@gmail.com>
2018-10-13 15:32:48 -0300

    Change text 3

--- test/test.js
+++ test/test.js
@@ -2,10 +2,10 @@ var request = require("supertest");
 var app = require("../index.js");
 
 describe("GET /", function() {
-  it("respond with Hola mundo 2", function(done) {
+  it("respond with Hola mundo 3", function(done) {
     //navigate to root and check the the response is "hello world"
     request(app)
       .get("/")
-      .expect("hHola mundo 2", done);
+      .expect("hHola mundo 3", done);
   });
 });
