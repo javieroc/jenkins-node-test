c9355f71395859425daad3cfc4fcabf46424dfb9
Javier Ocampo <ferroxido@gmail.com>
2018-10-13 15:19:54 -0300

    Change text

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 //Respond with "hello world" for requests that hit our root "/"
 app.get("/", function(req, res) {
-  res.send("hello world");
+  res.send("Hola mundo");
 });
 
 //listen to port 3000 by default
