e9ee14634b1ea43b2768a7ffc1af921699cf2593
Javier Ocampo <ferroxido@gmail.com>
2018-10-13 15:42:50 -0300

    Change text 4

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 //Respond with "hello world" for requests that hit our root "/"
 app.get("/", function(req, res) {
-  res.send("Hola mundo 3");
+  res.send("Hola mundo 4");
 });
 
 //listen to port 3000 by default
--- test/test.js
+++ test/test.js
@@ -2,10 +2,10 @@ var request = require("supertest");
 var app = require("../index.js");
 
 describe("GET /", function() {
-  it("respond with Hola mundo 3", function(done) {
+  it("respond with Hola mundo 4", function(done) {
     //navigate to root and check the the response is "hello world"
     request(app)
       .get("/")
-      .expect("hHola mundo 3", done);
+      .expect("hHola mundo 4", done);
   });
 });
