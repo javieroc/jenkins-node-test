7fc38c51e6e10bc4de03494a18d458fcb943c6c2
Javier Ocampo <ferroxido@gmail.com>
2018-10-13 15:46:04 -0300

    Change text 7

--- test/test.js
+++ test/test.js
@@ -2,10 +2,10 @@ var request = require("supertest");
 var app = require("../index.js");
 
 describe("GET /", function() {
-  it("respond with Hola mundo 6", function(done) {
+  it("respond with Hola mundo 7", function(done) {
     //navigate to root and check the the response is "hello world"
     request(app)
       .get("/")
-      .expect("hHola mundo 6", done);
+      .expect("hHola mundo 7", done);
   });
 });
