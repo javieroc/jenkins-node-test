3ff1c8a2556b652d707faeeb20a35066a72e4a8f
Javier Ocampo <ferroxido@gmail.com>
2018-10-13 16:02:24 -0300

    Change text 11

--- test/test.js
+++ test/test.js
@@ -7,10 +7,10 @@ describe("GET /", function() {
     done();
   });
 
-  it("respond with Hola mundo 10", function(done) {
+  it("respond with Hola mundo 11", function(done) {
     //navigate to root and check the the response is "hello world"
     request(server)
       .get("/")
-      .expect("Hola mundo 10", done);
+      .expect("Hola mundo 11", done);
   });
 });
